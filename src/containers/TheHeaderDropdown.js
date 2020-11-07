0c55a568a93463f602d81b3820216e9f19ec62b5
Juan Navas <navascruzf@gmail.com>
2020-11-07 13:38:26 -0400

    updated Cuentas and Tarjetas views 2

--- src/containers/TheHeaderDropdown.js
+++ src/containers/TheHeaderDropdown.js
@@ -4,6 +4,7 @@ import {
   CDropdownItem,
   CDropdownMenu,
   CDropdownToggle,
+  CImg,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
 import { useHistory } from "react-router-dom";
@@ -14,11 +15,11 @@ const TheHeaderDropdown = () => {
     <CDropdown inNav className="c-header-nav-items mx-2" direction="down">
       <CDropdownToggle className="c-header-nav-link" caret={false}>
         <div className="c-avatar">
-          {/* <CImg
-            src={"avatars/6.jpg"}
+          <CImg
+            src="https://www.gravatar.com/avatar?s=200&d=mp"
             className="c-avatar-img"
             alt="admin@bootstrapmaster.com"
-          /> */}
+          />
         </div>
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
